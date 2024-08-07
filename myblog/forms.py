cea2a87c18d1e6f1e302e5d418e060090f69b363
SpiritInMotion <maikemvictorine@gmail.com>
2024-08-07 16:38:14 +0200

    edited design and content

--- myblog/forms.py
+++ myblog/forms.py
@@ -9,7 +9,27 @@ class BlogPostForm(forms.ModelForm):
 
 
 
+# # forms.py
+# from django import forms
+# from .models import Contact
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['full_name', 'organisation', 'email', 'phone', 'subject', 'message']
+        labels = {
+            'full_name': 'Full Name',
+            'organisation': 'Organisation',
+            'email': 'Email',
+            'phone': 'Phone',
+            'subject': 'Subject',
+            'message': 'Message',
+        }
+        widgets = {
+            'full_name': forms.TextInput(attrs={'placeholder': 'Your Full Name'}),
+            'organisation': forms.TextInput(attrs={'placeholder': 'Your Organisation'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Your Phone Number'}),
+            'subject': forms.TextInput(attrs={'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Your Message'}),
+        }
