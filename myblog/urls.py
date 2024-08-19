457106447c2cbf7e2d937172799e44f66c5e354d
SpiritInMotion <maikemvictorine@gmail.com>
2024-08-19 07:22:28 -0400

    adding logos

--- myblog/urls.py
+++ myblog/urls.py
@@ -15,7 +15,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),  # Make home page load on base URL
+    path('', views.home, name='home'),  # Make home page load on base URL
     path('blog/', views.blog_list, name='blog_list'),
     path('<int:pk>/', views.blog_detail, name='blog_detail'),
     path('create/', views.blog_create, name='blog_create'),
