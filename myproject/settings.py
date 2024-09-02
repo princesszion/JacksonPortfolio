480d4b357a005a1a0d57841db0b732f2ac4a4c42
SpiritInMotion <maikemvictorine@gmail.com>
2024-09-02 06:47:30 -0400

    added config for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nolimit-jacksonapp.fjyxll.easypanel.host/','jacksonandalice.com']
 
 
 # Application definition
